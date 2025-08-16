0d72d64fb3eb8940710f3ec3d09525c2c12550f0
MaxHuesca <ismadlsh@lcg.unam.mx>
2025-08-15 22:40:37 -0600

    Ejercicio_2.py v1.0

--- src/Ejercicio_2.py
+++ src/Ejercicio_2.py
@@ -1,35 +1,37 @@
 # Creacion de la clase gen con 3 atributos y 3 metodos 
 
-class Gen(): 
-    #atributos del gen 
-    longitud=0 
-    Secuencia=""
-    cadena=""
-    start=0 
-    stop=0 
+class Gen():  
+    bases= {
+        "A":"U", 
+        "T":"A",
+        "C":"G",
+        "G":"C"
+    }
     
     def __init__(self, tamaño, Secuencia, cadena, inicio, fin):
         self.longitud= tamaño
-        self.Secuencia=Secuencia.upper
+        self.Secuencia="".join(Secuencia)
         self.cadena= cadena 
         self.start= inicio
         self.stop= fin
         pass
     
     def transcirbir(self): 
+        RNA=self.Secuencia
         if self.cadena=="foward": 
-            return [secuencia.replace("T", "U").strip() for secuencia in self.Secuencia]
+            RNA=RNA.replace("T","U")
+            return RNA
         else: 
-            self.Secuencia.replace("A","G")
-            self.Secuencia.replace("G","A")
-            self.Secuencia.replace("T","A")
-            self.Secuencia.replace("A","U")
-            return self.Secuencia[-1::]
+            for complemento in self.bases:
+                RNA=RNA.replace(complemento, self.bases[complemento])      
+            return RNA[::-1]
     
-    def marcos_lectura(self, marco):
-        marco -=1
-        codones= self.Secuencia[marco:] 
-        return [self.Secuencia[pos:pos+3] for pos in len(codones)/3] 
+    def marcos_lectura(self):
+        codones= [self.Secuencia[i:] for i in range(0,3)] 
+        marcos=[[marco[pos:pos+3] for pos in range(0,round(len(marco)/3))] for marco in codones]
+        
+        return marcos 
+                
     
     def GC(self):
         return self.Secuencia.count("G")+self.Secuencia.count("C")/len(self.Secuencia)
@@ -37,8 +39,8 @@ class Gen():
 
 lacY=Gen(96,"ATCGTTTAAACGACTTTTACGGGGATTTACCCATTTTAGGCATTTTATCGATCGGGATCCACTGCATTACCCGCTACGCTTAGTCGATGCTGATCG", "reverse",20167,20263) 
 
-lacY_RNA=lacY.transcirbir                
-codones=lacY.marcos_lectura
-lac_Y_GC=lacY.GC
+lacY_RNA=lacY.transcirbir()               
+codones=lacY.marcos_lectura()
+lac_Y_GC=lacY.GC()
 
 print(f"El transcrito de lacY es {lacY_RNA}, sus codones lucen: {codones} y su contenido de GC es de: {lac_Y_GC}")
