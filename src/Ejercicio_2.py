badde866cf17a8d42d01daaa92893b4659759928
MaxHuesca <ismadlsh@lcg.unam.mx>
2025-08-15 19:47:52 -0600

    Avance Ejercicio_2.py

--- src/Ejercicio_2.py
+++ src/Ejercicio_2.py
@@ -3,7 +3,7 @@
 class Gen(): 
     #atributos del gen 
     longitud=0 
-    Secuencia=[""]
+    Secuencia=""
     cadena=""
     start=0 
     stop=0 
@@ -17,7 +17,14 @@ class Gen():
         pass
     
     def transcirbir(self): 
-        return [secuencia.replace("T", "U").strip() for secuencia in self.Secuencia]
+        if self.cadena=="foward": 
+            return [secuencia.replace("T", "U").strip() for secuencia in self.Secuencia]
+        else: 
+            self.Secuencia.replace("A","G")
+            self.Secuencia.replace("G","A")
+            self.Secuencia.replace("T","A")
+            self.Secuencia.replace("A","U")
+            return self.Secuencia[-1::]
     
     def marcos_lectura(self, marco):
         marco -=1
@@ -28,7 +35,10 @@ class Gen():
         return self.Secuencia.count("G")+self.Secuencia.count("C")/len(self.Secuencia)
     
 
-                
+lacY=Gen(96,"ATCGTTTAAACGACTTTTACGGGGATTTACCCATTTTAGGCATTTTATCGATCGGGATCCACTGCATTACCCGCTACGCTTAGTCGATGCTGATCG", "reverse",20167,20263) 
 
+lacY_RNA=lacY.transcirbir                
+codones=lacY.marcos_lectura
+lac_Y_GC=lacY.GC
 
-    
+print(f"El transcrito de lacY es {lacY_RNA}, sus codones lucen: {codones} y su contenido de GC es de: {lac_Y_GC}")
